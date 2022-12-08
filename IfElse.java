7871335ed6d7b056f1f2659a3b7bc31061914df3
HugoSanche <107966476+HugoSanche@users.noreply.github.com>
2022-12-07 23:12:45 -0600

    Add files via upload
    Exercises with boolean, comparations and codicional If

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] Args){
+        int grade=45;
+        if (grade>=50){
+            System.out.println("Great, you passed!");
+        }
+        else{
+            System.out.println("Study hard next time!");
+        }
+    }
+}
