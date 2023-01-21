7ab95c1160ef6c0287fa58bac25332f3f0f6a041
HugoSanche <107966476+HugoSanche@users.noreply.github.com>
2023-01-20 22:56:09 -0600

    Add files via upload
    Remove working with arrays and add movies with file. Also add throw and runtime validations and arguments validations.

--- Movie.java
+++ Movie.java
@@ -1,48 +1,63 @@
-public class Movie {
-
+class Movie{
     private String name;
     private String format;
     private double rating;
+
+
 
     public Movie(String name, String format, double rating) {
         this.name = name;
         this.format = format;
         this.rating = rating;
-    }    
-
-
-    public Movie(Movie source) {
-        this.name = source.name;
-        this.format = source.format;
-        this.rating = source.rating;
+    }
+    public Movie(Movie sources){
+        this.name=sources.name;
+        this.format=sources.format;
+        this.rating=sources.rating;
+    }
+    @Override
+    public String toString(){
+        return +this.rating+"\t"+this.format+"  "+this.name;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-    
-    public String getFormat() {
-        return format;
+        if (name==null || name.isBlank()){
+            throw new IllegalArgumentException("Ilegal name");
+            
+        }else{
+            this.name = name;
+        }
+      
     }
 
-    public double getRating() {
-        return rating;
+    public String getFormat() {
+        return this.format;
     }
 
     public void setFormat(String format) {
-        this.format = format;
+        if (format==null || format.isBlank()){
+            throw new IllegalArgumentException("Error in format");
+        }else{
+            this.format = format;
+        }
+        
+    }
+
+    public double getRating() {
+
+        return this.rating;
     }
 
     public void setRating(double rating) {
+        if (rating <0 || rating>10){
+            throw new IllegalArgumentException("Argument between 0 and 10");
+        }
         this.rating = rating;
-    }
 
-    public String toString() {
-        return this.rating + "\t" + this.format + "\t\t" + this.name + "";
-    }    
+    }
 
 }
--- Store.java
+++ Store.java
@@ -1,25 +1,28 @@
-public class Store {
+import java.util.ArrayList;
 
-    private Movie[] movies;
+public class Store {
+   ArrayList<Movie> movies;
 
     public Store() {
-        this.movies = new Movie[10];
+        this.movies = new ArrayList<Movie>();
     }
 
-    public Movie getMovie(int index) {
-        return new Movie(this.movies[index]);
-    }
+public Movie getMovie(int index){
+    return new Movie(this.movies.get(index));
+}
 
-    public void setMovie(int index, Movie movie) {
-        this.movies[index] = new Movie(movie);
-    }
+public void setMovie(int index, Movie movie){
+    this.movies.set(index, new Movie(movie));
 
-    public String toString() {
-        String temp = "";
-        for (Movie movie : movies) {
-            temp += movie.toString() + "\n";
-        }
-        return temp;
+}
+public void addMovie(Movie movie) {
+    this.movies.add(new Movie(movie));
+}
+public String toString() {
+    String temp = "";
+    for (Movie movie : movies) {
+        temp += movie.toString() + "\n";
     }
-
+    return temp;
+}
 }
